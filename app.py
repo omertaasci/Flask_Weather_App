de6218d61b98199e13ce5081724a24e195d38cfb
Ömer Taşcı <65555672+omertaasci@users.noreply.github.com>
2023-12-13 13:05:17 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+# Install the required packages using:
+# pip install Flask requests
+
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = 'b8a922c1fc0b8ef2f698fea10b957f83'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather(city)
+
+    return render_template('index.html', weather_data=weather_data)
+
+def get_weather(city):
+    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    if response.status_code == 200:
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+        return weather
+    else:
+        return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
